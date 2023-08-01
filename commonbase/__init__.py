f85d390f767385209143d254790be9548acd576f
Brandon Meyerowitz <brandon@commonbase.com>
2023-08-01 10:04:41 +0200

    fix: export TruncationConfig from __init__.py

--- commonbase/__init__.py
+++ commonbase/__init__.py
@@ -2,6 +2,7 @@ from commonbase.completion import Completion
 from commonbase.exceptions import CommonbaseApiException, CommonbaseException
 from commonbase.chat_context import ChatContext, ChatMessage
 from commonbase.provider_config import ProviderConfig, OpenAIParams, AnthropicParams
+from commonbase.truncation_config import TruncationConfig
 
 __all__: [
     "Completion",
@@ -12,4 +13,5 @@ __all__: [
     "ProviderConfig",
     "OpenAIParams",
     "AnthropicParams",
+    "TruncationConfig",
 ]  # type: ignore
